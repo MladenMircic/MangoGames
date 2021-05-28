012ed319edb12a2e5c33970f3e8ede66ad66c3a8
Kosta Dimitrijevic <kole.dimi99@gmail.com>
2021-05-28 20:36:26 +0200

    Sredjivanje fajlova

--- Dokumentacija/Faza5/projekat/app/Views/pages/game.php
+++ Dokumentacija/Faza5/projekat/app/Views/pages/game.php
@@ -1,46 +1,22 @@
-<script>
-    let songDuration;
-    function playAudio() {
-        let request = new XMLHttpRequest();
-        request.open("GET", "<?= base_url("{$songToBePlayed->path}") ?>", true);
-        request.responseType = "blob";
-        let audio;
-        request.onload = function() {
-            if (this.status == 200) {
-                audio = new Audio(URL.createObjectURL(this.response));
-                audio.load();
-                audio.onloadedmetadata = function () {
-                    songDuration = audio.duration;
-                    audio.currentTime = Math.random() * songDuration;
-                    audio.play();
-                }
-            }
-        }
-        request.send();
-    }
-
-    $(document).ready(function () {
-        playAudio();
-
-        $(".guess").click(function () {
-           if ($(this).attr("value") === "<?= $songToBePlayed->name ?>")
-               alert("Pogodak!");
-           else
-               alert("Promasaj!!!!!!");
-        });
-    });
-</script>
-
 <table class="table userInterfaceForm">
-    <?php
-        shuffle($songs);
-        foreach($songs as $song) {
-            $song = str_replace('\'', '&apos;', $song);
-            echo "<tr>
+    <tr>
         <td>
-            <input class='btn btn-dark guess' type='button' value='$song'>
+            <input type="button" class="btn btn-dark">
         </td>
-    </tr>";
-        }
-    ?>
+    </tr>
+    <tr>
+        <td>
+            <input type="button" class="btn btn-dark">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type="button" class="btn btn-dark">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type="button" class="btn btn-dark">
+        </td>
+    </tr>
 </table>
